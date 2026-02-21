dacdbcbefcc0749e87a67f1e0f98e7828bdd9f07
abhishekaland29-cpu <abhishekaland29@gmail.com>
2026-02-21 20:43:14 +0530

    Add files via upload

---
+++ Task-1_ Client_Portfolio/file.js
@@ -0,0 +1,30 @@
+const wrapper = document.querySelector('.projects-wrapper');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let counter = 0;
+
+nextBtn.addEventListener('click', () => {
+    const card = document.querySelector('.project-info');
+    const cardWidth = card.clientWidth + 20; 
+    const maxScroll = wrapper.scrollWidth - wrapper.parentElement.clientWidth;
+    
+    if ((counter + 1) * cardWidth <= maxScroll + cardWidth) {
+        counter++;
+        wrapper.style.transform = `translateX(${-cardWidth * counter}px)`;
+    } else {
+        // Agar aakhri card aa gaya toh wapas 0 par le jao (Optional Loop)
+        counter = 0;
+        wrapper.style.transform = `translateX(0px)`;
+    }
+});
+
+prevBtn.addEventListener('click', () => {
+    const card = document.querySelector('.project-info');
+    const cardWidth = card.clientWidth + 20;
+
+    if (counter > 0) {
+        counter--;
+        wrapper.style.transform = `translateX(${-cardWidth * counter}px)`;
+    }
+});
